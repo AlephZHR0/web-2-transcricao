76b8dc0e895ba0078c303689084c7bb7468d1d38
Nevinho10 <Nevinho1011@gmail.com>
2023-12-06 15:14:17 -0300

    Arrumando as pastas

---
+++ usuario_frontend-main/Menu/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const enviarBtn = document.getElementById("enviar");
+    const enviarUrlBtn = document.getElementById("enviarUrl");
+    const textoInput = document.getElementById("texto");
+    const chatContainer = document.getElementById("chatContainer");
+
+    enviarBtn.addEventListener("click", function () {
+        enviarMensagem();
+    });
+
+    enviarUrlBtn.addEventListener("click", function () {
+        const url = prompt("Digite a URL:");
+        if (url) {
+            enviarMensagem(`<a href="${url}" target="_blank">${url}</a>`);
+        }
+    });
+
+    function enviarMensagem(mensagem = "") {
+        if (mensagem.trim() !== "") {
+            const novaMensagem = document.createElement("p");
+            novaMensagem.innerHTML = mensagem;
+            chatContainer.appendChild(novaMensagem);
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+            textoInput.value = "";
+        }
+    }
+});
